f7c08c615e3347160df361d515e590d505289a2d
Alexander Jacks and Matt Wilkin <alexander@>
2016-06-21 11:35:03 -0700

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+// backend
+function Plant(sunlight, time, ) {
+  // , time, space, care, color
+  // this.plantName = name;
+  this.plantSun = sunlight;
+  // this.plantType = type;
+  this.plantTime = time;
+  this.plantSpace = space;
+  this.plantMaintenance = maintenance;
+  // this.plantColor = color;
+}
+
+
 // var asparagusPlant = {
 //   plantName: "asparagus",
 //   plantSun: "partial sun",
@@ -86,3 +99,34 @@ var marigoldPlant = {
 //   plantMaintanence: "low",
 //   plantTime: "late summer"
 // }
+
+var myFriend = function();
+  for ( i = 0 ; i < plantFriends.length ; i++ ) {
+    if ($(".DROPDOWN").val() === plantFriends[i] ){
+      var removed = plantFriends.splice(i, 0);
+      return plantFrinds;
+    } else if ($".DROPDOWN".val() === plantFriendsTwo[i] ){
+      var removed = plantFriendsTwo.splice(i,0);
+    }
+  }
+
+var plantFriends = ["tomato", "bean-pole", "marigold"];
+var plantFriendsTwo = ["", "", ""]
+
+// Plant.prototype.plantFacts = function() {
+//   return "please give this " + this.plantName + " about " + plantSpace
+// }
+
+$(document).ready(function(){
+
+  $("#bean-pole").click(function(event){
+    event.preventDefault();
+    $(".modal-plant-title").text(beanPolePlant.plantName);
+    $(".sun").text(this.plantTime);
+    $(".time").text(this.plantTime);
+    $("#plant-modal").modal("show");
+  });
+
+});
+
+//
