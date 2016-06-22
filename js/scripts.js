f47705b0ae2b8f363e82ce8f705809c505ddccd7
Matt Wilkin and Terri Bradley <matt@>
2016-06-22 12:13:10 -0700

    create css classes for compainion growing divs

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,45 @@
 function Plant(name, sunlight, time, space, maintenance) {
   this.plantName = name;
   this.plantSun = sunlight;
-  // this.plantType = type;
   this.plantTime = time;
   this.plantSpace = space;
   this.plantMaintenance = maintenance;
-  // this.plantColor = color;
 }
 
+// Plant.prototype.plantinfo = function() {
+//   var elem = document.getElementById("bean-pole");
+//   $("#plant-index").click(function(){
+//     $(".modal-plant-title").text(this.name);
+//     $(".sun").text(this.sunlight);
+//     $(".space").text(this.space);
+//     $(".maintenance").text(this.maintenance);
+//     $(".time").text(this.time);
+//     $("#plant-modal").modal("show");
 
+var plantidArray=["artichoke", "asparagus","bean-pole","beet", "brussel-sprout", "cabbage", "carrot", "cauliflower", "cucumber", "garlic", "kale", "leek", "lettuce", "onion", "pea", "radish", "spinach", "tomato", "parsley", "sage", "thyme", "marigold" ];
+
+var plantObjectArray= [artichokePlant, asparagusPlant, beanPolePlant, beetPlant, brusselPlant, cabbagePlant, carrotPlant, cauliflowerPlant, cumcumberPlant, garlicPlant, kalePlant, leekPlant, lettucePlant, onionPlant, peaPlant, radishPlant, spinachPlant, tomatoPlant, parsleyPlant, sagePlant, thymePlant, marigoldPlant];
+
+var plantClick = (function(id, plantObject) {
+$(id).click(function() {
+  $(".modal-plant-title").text(plantObject.name);
+  $(".sun").text(plantObject.sunlight);
+  $(".time").text(plantObject.time);
+  $(".space").text(plantObject.space);
+  $(".maintenance").text(plantObject.maintenance);
+  $("#plant-modal").modal("show");
+
+for (i = 0; i < plantidArray.length; i ++){
+  plantClick(plantidArray[i], plantObjectArray[i]);
+}
+});
+});
 var asparagusPlant = {
   name: "ASPARAGUS",
   sunlight: "Partial sun",
-  type: "Vegetable",
+  time: "Spring",
   space: "1 plant per foot",
-  maintanence: "Medium",
-  time: "Spring"
+  maintenance: "Medium",
 }
 var beanPolePlant = {
   name:"BEAN-POLES",
@@ -98,6 +122,102 @@ var basilPlant = {
   maintanence:"Low",
   time:"Late summer"
 }
+var artichokePlant = {
+  name: "ARTICHOKE",
+  sunlight: "Full sun",
+  type: "Vegetable",
+  space: "1 plant per foot",
+  maintanence: "HIGH",
+  time: "Early Summer"
+}
+var brusselPlant = {
+  name: "BRUSSEL SPROUTS",
+  sunlight: "Partial sun",
+  type: "Vegetable",
+  space: "2 plant per foot",
+  maintanence: "Medium",
+  time: "Late Summer"
+}
+var cabbagePlant = {
+  name: "CABBAGE",
+  sunlight: "Partial sun",
+  type: "Vegetable",
+  space: "1 plant per foot",
+  maintanence: "Medium",
+  time: "Late Summer"
+}
+var cauliflowerPlant = {
+  name: "CAULIFLOWER",
+  sunlight: "Full sun",
+  type: "Vegetable",
+  space: "1 plant per foot",
+  maintanence: "High",
+  time: "Early Summer"
+}
+var cucumberPlant = {
+  name: "CUCUMBER",
+  sunlight: "Full sun",
+  type: "Vegetable",
+  space: "1 plant per foot",
+  maintanence: "Low",
+  time: "Early Summer"
+}
+var garlicPlant = {
+  name: "garlic",
+  sunlight: "Partial sun",
+  type: "Vegetable",
+  space: "2 plant per foot",
+  maintanence: "Low",
+  time: "Spring"
+}
+var leekPlant = {
+  name: "LEEKS",
+  sunlight: "Full sun",
+  type: "Vegetable",
+  space: "1 plant per foot",
+  maintanence: "Medium",
+  time: "Late Summer"
+}
+var parsleyPlant = {
+  name: "PARSLEY",
+  sunlight: "Full sun",
+  type: "Plant",
+  space: "2 plant per foot",
+  maintanence: "Low",
+  time: "Spring"
+}
+var radishPlant = {
+  name: "radish",
+  sunlight: "Partial sun",
+  type: "Vegetable",
+  space: "3 plant per foot",
+  maintanence: "Low",
+  time: "Spring"
+}
+var sagePlant = {
+  name: "ASPARAGUS",
+  sunlight: "Full sun",
+  type: "Plant",
+  space: "1 plant per foot",
+  maintanence: "Low",
+  time: "Spring"
+}
+var spinachPlant = {
+  name: "SPINACH",
+  sunlight: "Partial sun",
+  type: "Vegetable",
+  space: "1 plant per foot",
+  maintanence: "Medium",
+  time: "Late Summer"
+}
+var thymePlant = {
+  name: "THYME",
+  sunlight: "Full sun",
+  type: "Plant",
+  space: "1 plant per foot",
+  maintanence: "Low",
+  time: "Spring"
+}
 
 var newList = [];
 
@@ -132,16 +252,13 @@ var plantFriendsSeven = ["spinach", "kale", "onion", "thyme"];
 var plantFriendsEight = ["carrots", "parsley", "leeks", "cabbage", "garlic"];
 
 $(document).ready(function(){
+ plantClick(ids, plantObjects);
 
-  $("#bean-pole").click(function(event){
-    event.preventDefault();
-    $(".modal-plant-title").text(beanPolePlant.name);
-    $(".sun").text(beanPolePlant.sunlight);
-    $(".space").text(beanPolePlant.space);
-    $(".maintenance").text(beanPolePlant.maintenance);
-    $(".time").text(beanPolePlant.time);
-    $("#plant-modal").modal("show");
+$("#dropdown1").change(function(){
+$("#gardenbox-one").addclass(plant-one);
+});
   });
+
 // FOR IN STATEMENT TO POPULATE MODAL USING A PROTOTYPE(spitData) EQUATION IS BELOW, BUT IT DOESNT WORK AS A PROTOTYPE YET
   // $("#bean-pole").click(function(event){
   //   event.preventDefault();
@@ -154,9 +271,9 @@ $(document).ready(function(){
 //
 // };
 
-  $("#dropdown1").change(function(event){
-    var myChoice = $("#dropdown1").val();
-    myFriend(myChoice);
-    box2Populate(newList);
-  });
-});
+  // $("#dropdown1").change(function(event){
+  //   var myChoice = $("#dropdown1").val();
+  //   myFriend(myChoice);
+  //   box2Populate(newList);
+//   });
+// });
