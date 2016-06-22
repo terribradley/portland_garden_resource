9340ee95cbf1910f6b24a084e95c2eafd7617252
Alexander Jacks and Julia Dickey <julia@>
2016-06-22 10:07:35 -0700

    adds support for prototyped objects, can refactor arrays into value lists and allow objects to reference each other

--- js/scripts.js
+++ js/scripts.js
@@ -1,119 +1,144 @@
 // backend
-function Plant(name, sunlight, time, space, maintenance) {
+function Plant(name, sunlight, time, space, maintenance, friend) {
   this.plantName = name;
   this.plantSun = sunlight;
   // this.plantType = type;
   this.plantTime = time;
   this.plantSpace = space;
   this.plantMaintenance = maintenance;
+  this.friend = friend;
   // this.plantColor = color;
 }
 
+// CREATES BEAN-POLE OBJECT
+var beanPolePlant = new Plant ("BEAN-POLE", "Full sun", "Spring", "2 plants per foot", "low", ["tomato", "marigold", "peas", "radish"]);
+var asparagusPlant = new Plant ("ASPARAGUS", "Partial sun", "Spring", "1 plant per foot", "Medium", ["asparagus", "basil", "marigold", "parsley"]);
 
-var asparagusPlant = {
-  name: "ASPARAGUS",
-  sunlight: "Partial sun",
-  type: "Vegetable",
-  space: "1 plant per foot",
-  maintanence: "Medium",
-  time: "Spring"
-}
-var beanPolePlant = {
-  name:"BEAN-POLES",
-  sunlight:"Full sun",
-  type:"Vegetable",
-  space:"2 plants per foot",
-  maintenance:"Low",
-  time: "Spring"
-}
-var beetPlant = {
-  name:"BEETS",
-  sunlight:"Full sun",
-  type:"Vegetable",
-  space:"1 plants per foot",
-  maintanence:"Medium",
-  time:"Early-summer"
-}
-var carrotPlant = {
-  name:"CARROTS",
-  sunlight:"Partial sun",
-  type:"Vegetable",
-  space:"2 plants per foot",
-  maintanence:"Low",
-  time:"Early-summer"
-}
-var kalePlant = {
-  name:"KALE",
-  sunlight:"Full sun",
-  type:"Vegetable",
-  space:"1 plants per foot",
-  maintanence:"Medium",
-  time:"Spring"
-}
-var lettucePlant = {
-  name:"LETTUCE",
-  sunlight:"Full sun",
-  type:"Vegetable",
-  space:"2 plants per foot",
-  maintanence:"Low",
-  time:"Spring"
-}
-var onionPlant = {
-  name:"ONIONS",
-  sunlight:"Partial",
-  type:"Vegetable",
-  space:"2 plants per foot",
-  maintanence:"Low",
-  time:"Spring"
-}
-var peaPlant = {
-  name:"PEAS",
-  sunlight:"Full sun",
-  type:"Vegetable",
-  space:"3 plants per foot",
-  maintanence:"Low",
-  time:"Spring"
-}
-var tomatoPlant = {
-  name:"TOMATOES",
-  sunlight:"Full sun",
-  type:"Vegetable",
-  space:"1 plants per foot",
-  maintanence:"Medium",
-  time:"Late summer"
-}
-var marigoldPlant = {
-  name:"MARIGOLDS",
-  sunlight:"Full sun",
-  type:"Plant",
-  space:"3 plants per foot",
-  maintanence:"Low",
-  time:"Late summer"
-}
-var basilPlant = {
-  name:"BASIL",
-  sunlight:"Full sun",
-  type:"Plant",
-  space:"3 plants per foot",
-  maintanence:"Low",
-  time:"Late summer"
-}
 
-var newList = [];
+// var beanPolePlant = {
+//   name:"BEAN-POLES",
+//   sunlight:"Full sun",
+//   time: "Spring",
+//   space:"2 plants per foot",
+//   maintenance:"Low",
+//   // type:"Vegetable",
+//   friend: ["tomato", "marigold", "peas", "radish"]
+// }
+// var beetPlant = {
+//   name:"BEETS",
+//   sunlight:"Full sun",
+//   type:"Vegetable",
+//   space:"1 plants per foot",
+//   maintanence:"Medium",
+//   time:"Early-summer"
+// }
+// var carrotPlant = {
+//   name:"CARROTS",
+//   sunlight:"Partial sun",
+//   type:"Vegetable",
+//   space:"2 plants per foot",
+//   maintanence:"Low",
+//   time:"Early-summer"
+// }
+// var kalePlant = {
+//   name:"KALE",
+//   sunlight:"Full sun",
+//   type:"Vegetable",
+//   space:"1 plants per foot",
+//   maintanence:"Medium",
+//   time:"Spring"
+// }
+// var lettucePlant = {
+//   name:"LETTUCE",
+//   sunlight:"Full sun",
+//   type:"Vegetable",
+//   space:"2 plants per foot",
+//   maintanence:"Low",
+//   time:"Spring"
+// }
+// var onionPlant = {
+//   name:"ONIONS",
+//   sunlight:"Partial",
+//   type:"Vegetable",
+//   space:"2 plants per foot",
+//   maintanence:"Low",
+//   time:"Spring"
+// }
+// var peaPlant = {
+//   name:"PEAS",
+//   sunlight:"Full sun",
+//   type:"Vegetable",
+//   space:"3 plants per foot",
+//   maintanence:"Low",
+//   time:"Spring"
+// }
+// var tomatoPlant = {
+//   name:"TOMATOES",
+//   sunlight:"Full sun",
+//   type:"Vegetable",
+//   space:"1 plants per foot",
+//   maintanence:"Medium",
+//   time:"Late summer"
+// }
+// var marigoldPlant = {
+//   name:"MARIGOLDS",
+//   sunlight:"Full sun",
+//   type:"Plant",
+//   space:"3 plants per foot",
+//   maintanence:"Low",
+//   time:"Late summer"
+// }
+// var basilPlant = {
+//   name:"BASIL",
+//   sunlight:"Full sun",
+//   type:"Plant",
+//   space:"3 plants per foot",
+//   maintanence:"Low",
+//   time:"Late summer"
+// }
 
-function myFriend(choice) {
-  for ( var i = 0 ; i < plantFriends.length ; i++ ) {
-    if (choice === plantFriends[i] ){
-      plantFriends.splice(i, 1);
-      // alert(plantFriends);
-      newList.push(plantFriends);
+var friendsOfChosen = [];
+
+Plant.prototype.myFriend = function(choice) {
+  debugger;
+  for ( var i = 0 ; i < this.friend.length ; i++ ) {
+    if (choice === this.friend[i] ){
+      friendsOfChosen.push(this.friend);
+      console.log(friendsOfChosen);
+    } else {
+      alert("oops! no friends found");
     }
-    // else if ($("#dropdown1").val() === plantFriendsTwo[i] ) {
-    //   var removed = plantFriendsTwo.slice(i, 0);
-    //   alert(plantFriendsTwo);
-    // }
   }
 };
+
+// THIS FUNCTION WORKS TO REFERENCE THE FRIENDS
+// function myFriend(choice) {
+//   debugger;
+//   for ( var i = 0 ; i < beanPolePlant.friend.length ; i++ ) {
+//     if (choice === beanPolePlant.friend[i] ){
+//       friendsOfChosen.push(beanPolePlant.friend);
+//       console.log(friendsOfChosen);
+//     } else {
+//       alert("oops! no friends found");
+//     }
+//   }
+// };
+
+
+// WORKS TO REMOVE SELECTED ITEM FROM ARRAY
+// function myFriend(choice) {
+//   for ( var i = 0 ; i < plantFriends.length ; i++ ) {
+//     if (choice === plantFriends[i] ){
+//       plantFriends.splice(i, 1);
+//       // alert(plantFriends);
+//       newList.push(plantFriends);
+//     }
+//     // else if ($("#dropdown1").val() === plantFriendsTwo[i] ) {
+//     //   var removed = plantFriendsTwo.slice(i, 0);
+//     //   alert(plantFriendsTwo);
+//     // }
+//   }
+// };
 
 function box2Populate (inputfrombox1){
   for ( var i = 0 ; i < inputfrombox1.length ; i++ ) {
@@ -123,18 +148,20 @@ function box2Populate (inputfrombox1){
 };
 
 var plantFriends = ["tomato", "bean-pole", "marigold", "peas", "radish"];
-var plantFriendsTwo = ["kale", "onion", "peas", "brussel sprouts", "garlic", "cabbage"];
-var plantFriendsThree = ["califlower", "artichoke", "beets", "lettuce", "tomato"];
-var plantFriendsFour = ["cucumber", "basil", "marigold", "onion"];
-var plantFriendsFive = ["asparagus", "basil", "marigold", "parsley"];
-var plantFriendsSix = ["carrots", "lettuce", "sage", "radish", "beets"];
-var plantFriendsSeven = ["spinach", "kale", "onion", "thyme"];
-var plantFriendsEight = ["carrots", "parsley", "leeks", "cabbage", "garlic"];
+// var plantFriendsTwo = ["kale", "onion", "peas", "brussel sprouts", "garlic", "cabbage"];
+// var plantFriendsThree = ["califlower", "artichoke", "beets", "lettuce", "tomato"];
+// var plantFriendsFour = ["cucumber", "basil", "marigold", "onion"];
+// var plantFriendsFive = ["asparagus", "basil", "marigold", "parsley"];
+// var plantFriendsSix = ["carrots", "lettuce", "sage", "radish", "beets"];
+// var plantFriendsSeven = ["spinach", "kale", "onion", "thyme"];
+// var plantFriendsEight = ["carrots", "parsley", "leeks", "cabbage", "garlic"];
 
 $(document).ready(function(){
-
+// USER CLICKS ON BEAN POLE
   $("#bean-pole").click(function(event){
     event.preventDefault();
+
+// SENDS PLANT INFO TO MODAL
     $(".modal-plant-title").text(beanPolePlant.name);
     $(".sun").text(beanPolePlant.sunlight);
     $(".space").text(beanPolePlant.space);
@@ -155,8 +182,11 @@ $(document).ready(function(){
 // };
 
   $("#dropdown1").change(function(event){
-    var myChoice = $("#dropdown1").val();
-    myFriend(myChoice);
-    box2Populate(newList);
+    var chosenPlant = $("#dropdown1").val();
+
+    debugger;
+    beanPolePlant.myFriend(chosenPlant);
+    // myFriend(chosenPlant);
+    box2Populate(friendsOfChosen);
   });
 });
