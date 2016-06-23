528af928a6718942bf7560f08d99ce3457b6cb17
Alexander Jacks and Julia Dickey <julia@>
2016-06-22 17:04:18 -0700

    adds vegetable objects

--- js/scripts.js
+++ js/scripts.js
@@ -11,18 +11,36 @@ function Plant(name, sunlight, time, space, maintenance, friend) {
 }
 
 // CREATES VEGGIE OBJECTS
-var beans = new Plant ("beans", "Full sun", "Spring", "2 plants per foot", "low", ["tomato", "marigold", "peas", "radish"]);
+var beans = new Plant ("beans", "Full Sun", "Spring", "2 plants per foot", "Low", ["tomato", "marigold", "peas", "radish"]);
 
-var asparagus = new Plant ("asparagus", "Partial sun", "Spring", "1 plant per foot", "Medium", ["asparagus", "basil", "marigold", "parsley"]);
+var asparagus = new Plant ("asparagus", "Partial Sun", "Spring", "1 plant per foot", "Medium", ["asparagus", "basil", "marigold", "parsley"]);
 
-var allPlants = [ beans, asparagus ];
+var beets = new Plant ("beets","Full Sun","Early-summer","1 plants per foot","Medium", );
+
+var carrots = new Plant ("carrots","Partial Sun","Early-summer","2 plants per foot","Low", );
+
+var kale = new Plant ("kale","Full Sun","Spring","1 plants per foot","Medium");
+
+var lettuce = new Plant ("lettuce","Full Sun","Spring","2 plants per foot","Low");
+
+var onions = new Plant ("onions","Partial Sun","Spring","2 plants per foot","Low");
+
+var peas = new Plant ("peas","Full Sun","Spring","3 plants per foot","Low"
+);
+
+var tomatoes = new Plant ("tomatoes","Full Sun","Late summer","1 plants per foot","Medium");
+
+var marigolds = new Plant ("marigolds","Full Sun","Late summer","3 plants per foot","Low");
+
+var basil = new Plant ("basil","Full Sun","Late summer","3 plants per foot","Low");
+
+var allPlants = [ beans, asparagus, carrots, , , , , , , , , ];
 
 var chosenPlantObject;
 var indexPlantObject;
 
 // THIS FUCKING WORKS!!!!! INPUT USER STRING FROM CLICK, RETURNS OBJECT OF THE VEGGIE CHOSEN
 function searchForPlantObject(inputName){
-  debugger;
   for (var i = 0 ; i < allPlants.length ; i++){
     if (inputName === (allPlants[i].plantName)) {
       return allPlants[i];
@@ -30,23 +48,10 @@ function searchForPlantObject(inputName){
   }
 };
 
-// NOT WORKING, SORTING THROUGH
-// function searchForPlantObject(plantName){
-//   var plantObject;
-//   allPlants.forEach(function(plantObject) {
-//     debugger;
-//   // for (this.name in allPlants) {
-//     if (plantName === plantObject.name) {
-//       return plantObject;
-//       alert(plantObject);
-//     }
-//   });
-  // }
-// };
 // DETAILS ABOUT VEGGIE OBJECTS
 // var beanPolePlant = {
 //   name:"BEAN-POLES",
-//   sunlight:"Full sun",
+//   sunlight:"Full Sun",
 //   time: "Spring",
 //   space:"2 plants per foot",
 //   maintenance:"Low",
@@ -55,7 +60,7 @@ function searchForPlantObject(inputName){
 // }
 // var beetPlant = {
 //   name:"BEETS",
-//   sunlight:"Full sun",
+//   sunlight:"Full Sun",
 //   type:"Vegetable",
 //   space:"1 plants per foot",
 //   maintanence:"Medium",
@@ -63,7 +68,7 @@ function searchForPlantObject(inputName){
 // }
 // var carrotPlant = {
 //   name:"CARROTS",
-//   sunlight:"Partial sun",
+//   sunlight:"Partial Sun",
 //   type:"Vegetable",
 //   space:"2 plants per foot",
 //   maintanence:"Low",
@@ -71,7 +76,7 @@ function searchForPlantObject(inputName){
 // }
 // var kalePlant = {
 //   name:"KALE",
-//   sunlight:"Full sun",
+//   sunlight:"Full Sun",
 //   type:"Vegetable",
 //   space:"1 plants per foot",
 //   maintanence:"Medium",
@@ -79,7 +84,7 @@ function searchForPlantObject(inputName){
 // }
 // var lettucePlant = {
 //   name:"LETTUCE",
-//   sunlight:"Full sun",
+//   sunlight:"Full Sun",
 //   type:"Vegetable",
 //   space:"2 plants per foot",
 //   maintanence:"Low",
@@ -95,7 +100,7 @@ function searchForPlantObject(inputName){
 // }
 // var peaPlant = {
 //   name:"PEAS",
-//   sunlight:"Full sun",
+//   sunlight:"Full Sun",
 //   type:"Vegetable",
 //   space:"3 plants per foot",
 //   maintanence:"Low",
@@ -103,7 +108,7 @@ function searchForPlantObject(inputName){
 // }
 // var tomatoPlant = {
 //   name:"TOMATOES",
-//   sunlight:"Full sun",
+//   sunlight:"Full Sun",
 //   type:"Vegetable",
 //   space:"1 plants per foot",
 //   maintanence:"Medium",
@@ -111,7 +116,7 @@ function searchForPlantObject(inputName){
 // }
 // var marigoldPlant = {
 //   name:"MARIGOLDS",
-//   sunlight:"Full sun",
+//   sunlight:"Full Sun",
 //   type:"Plant",
 //   space:"3 plants per foot",
 //   maintanence:"Low",
@@ -119,7 +124,7 @@ function searchForPlantObject(inputName){
 // }
 // var basilPlant = {
 //   name:"BASIL",
-//   sunlight:"Full sun",
+//   sunlight:"Full Sun",
 //   type:"Plant",
 //   space:"3 plants per foot",
 //   maintanence:"Low",
@@ -170,16 +175,6 @@ function box2Populate (arrayofFriends){
   }
 };
 
-// ORIGINAL FRIEND ARRAYS -- TO BE DELETED
-// var plantFriends = ["tomato", "bean-pole", "marigold", "peas", "radish"];
-// var plantFriendsTwo = ["kale", "onion", "peas", "brussel sprouts", "garlic", "cabbage"];
-// var plantFriendsThree = ["califlower", "artichoke", "beets", "lettuce", "tomato"];
-// var plantFriendsFour = ["cucumber", "basil", "marigold", "onion"];
-// var plantFriendsFive = ["asparagus", "basil", "marigold", "parsley"];
-// var plantFriendsSix = ["carrots", "lettuce", "sage", "radish", "beets"];
-// var plantFriendsSeven = ["spinach", "kale", "onion", "thyme"];
-// var plantFriendsEight = ["carrots", "parsley", "leeks", "cabbage", "garlic"];
-
 $(document).ready(function(){
 // USER CLICKS ON VEGGIE, IDENTIFIES HTML VALUE
   $("ul.clicked li").click(function(event){
@@ -200,18 +195,6 @@ $(document).ready(function(){
 // SENDS PLANT INFO TO MODAL
 
 
-// FOR IN STATEMENT TO POPULATE MODAL USING A PROTOTYPE(spitData) EQUATION IS BELOW, BUT IT DOESNT WORK AS A PROTOTYPE YET
-  // $("#bean-pole").click(function(event){
-  //   event.preventDefault();
-  //   for (var prop in beanPolePlant) {
-  //     $("#plant-properties").append(prop, ": ", beanPolePlant[prop], "<br>");
-  //   }
-  // });
-
-// Plant.prototype.spitData = function () {
-//
-// };
-
   $("#dropdown1").change(function(event){
     var chosenPlant = $("#dropdown1").val();
 
@@ -219,7 +202,6 @@ $(document).ready(function(){
 
     var chosenPlantObject = searchForPlantObject(chosenPlant);
     chosenPlantObject.myFriend(chosenPlant);
-    // myFriend(chosenPlant);
     box2Populate(friendsOfChosen);
   });
 });
